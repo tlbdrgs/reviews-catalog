711bd98d7f8462a8f658b56b4392748d4942f304
Dragos <talaba.dragos@gmail.com>
2025-11-24 17:55:02 +0200

    search implemented

--- frontend/reviews-react/src/pages/CatalogPage.tsx
+++ frontend/reviews-react/src/pages/CatalogPage.tsx
@@ -1,10 +1,12 @@
 import { useProducts } from "../service/products";
 import ProductCard from "../components/ProductCard";
+import { useState } from "react";
 import { Fragment } from "react";
 import { Input } from "@/components/input";
 
 export default function CatalogPage() {
     const { products, isPending, isError } = useProducts();
+    const [searchInput, setSearchInput] = useState("");
 
     if (isPending) {
         return <div>Loading products...</div>;
@@ -14,21 +16,34 @@ export default function CatalogPage() {
         return <div>Failed to load products</div>;
     }
 
+    function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        setSearchInput(event.target.value);
+    }
+    
+    const filteredProducts = products?.filter((product) => {
+        return product.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+               product.description.toLowerCase().includes(searchInput.toLowerCase());
+    });
+
     return (
         <>
-        <div>
-            <div className="mx-5 mb-20">
+        <div className="bg-stone-100">
+            <div className="mx-5 pb-20">
                 <h1 className="text-center mb-15 text-3xl uppercase font-semibold">Product Catalog</h1>
-                <Input placeholder="Search products..." className="w-full max-w-md mb-10 mx-auto block" />
+                <Input onChange={handleSearch} placeholder="Search products..." className="w-full max-w-md mb-10 mx-auto block"/>
                 <div className="grid grid-cols-4 gap-10">
-                    {products?.map((product, index) => (
-                        <Fragment key={product.id}>
-                            <ProductCard product={product} />
-                            {(index + 1) % 4 === 0 && index !== products.length - 1 && (
-                                <div className='col-span-4 my-8 border-b-2 border-gray-200'></div>
-                            )}
-                        </Fragment>
-                    ))}
+                    {filteredProducts && filteredProducts.length === 0 ? (
+                        <div className="col-span-4 text-center text-gray-500">No products matching: "{searchInput}"</div>
+                    ) : (
+                        filteredProducts?.map((product, index) => (
+                            <Fragment key={product.id}>
+                                <ProductCard product={product} />
+                                {(index + 1) % 4 === 0 && index !== filteredProducts.length - 1 && (
+                                    <div className='col-span-4 my-8 border-b-2 border-gray-200'></div>
+                                )}
+                            </Fragment>
+                        ))
+                    )}
                 </div>
             </div>
         </div></>
